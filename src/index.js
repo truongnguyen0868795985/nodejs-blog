e0936dec7bdc09f0ca275ac8ceaf0b56ef681faa
DESKTOP-39MUHOT\Truong Nguyen <truongnguyen0868795985@gmail.com>
2021-07-26 18:50:48 +0700

    custom express handlebars extinsion to hbs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const morgan = require('morgan')
-const handlebars  = require('express-handlebars');
+const exphbs  = require('express-handlebars');
 const path = require('path')
 const app = express()
 const port = 3000
+
+// 
+app.use(express.urlencoded())
+app.use(express.json)
 
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // View engine - Handlebars
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'))
+
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Logger
 // app.use(morgan('combined'))
 
 // Routes
 app.get('/', (req, res) => {
-  console.log('path', path.join(__dirname, 'public'))
+  console.log(req.query.hihi);
   res.render('home');
 })
 
